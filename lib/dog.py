0cc91fe52174779dd4cca85c0ef8510d41a05cca
Manasseh Chege <chegemanasseh2@gmail.com>
2024-10-10 23:37:09 +0300

    Added the code for the person and the Dog class to pass the tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 
 APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+    "Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle",
+    "French Bulldog", "Pug", "Pointer"
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Mastiff"):
+        # Use the property setters
+        self.name = name  # This will invoke the name setter
+        self.breed = breed  # This will invoke the breed setter
+
+    # Property for name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = "Dog"  # Set to default name if invalid
+
+    # Property for breed
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = "Mastiff"  # Set to default breed if invalid
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Admin"):
+        # Use the property setters
+        self.name = name  # Invokes the name setter
+        self.job = job  # Invokes the job setter
+
+    # Property for name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # Convert to title case and store
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None  # Set to None if the name is invalid
+
+    # Property for job
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job = None  # Set to None if the job is invalid
